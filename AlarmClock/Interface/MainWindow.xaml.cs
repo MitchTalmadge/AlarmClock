38453dee046103e8db97f09b63d0afea02f0f8d5
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-04-26 17:10:31 -0600

    Moved music methods into MusicPlayer.cs

--- AlarmClock/Interface/MainWindow.xaml.cs
+++ AlarmClock/Interface/MainWindow.xaml.cs
@@ -15,16 +15,12 @@ namespace AlarmClock.Interface
     /// </summary>
     public partial class MainWindow
     {
-
         /// <summary>
         /// The wake up detector, which determines when the user has woken up.
         /// </summary>
         private WakeUpDetector _wakeUpDetector;
 
-        /// <summary>
-        /// The audio output handler.
-        /// </summary>
-        private WaveOut _waveOut;
+        private MusicPlayer _musicPlayer;
 
         public MainWindow()
         {
@@ -40,12 +36,14 @@ namespace AlarmClock.Interface
             _wakeUpDetector.WakeUpConfirmedEvent += WakeUpDetectorOnWakeUpConfirmedEvent;
             _wakeUpDetector.WakeUpProgressEvent += WakeUpDetectorOnWakeUpProgressEvent;
 
+            _musicPlayer = new MusicPlayer();
+
             if (_wakeUpDetector.StartDetection())
             {
                 Background = new SolidColorBrush(Colors.Black);
 
                 //Begin playing alarm clock sound on loop
-                PlayAudio(AssetFinder.AssetToUri("audio/AlarmClock.mp3"), true);
+                _musicPlayer.PlayMp3Asset("AlarmClock.mp3", true);
             }
             else //Could not load Kinect Sensor. Just skip it.
             {
@@ -54,51 +52,24 @@ namespace AlarmClock.Interface
             }
         }
 
+        private void DoWakeupRoutine()
+        {
+            TimeSpan totalTime = _musicPlayer.PlayMp3Asset("Intro.mp3", false); //Start playing intro music.
+            Background = new SolidColorBrush(Colors.White);
+            BackgroundImage.Visibility = Visibility.Visible;
+            BackgroundImage.Source = new BitmapImage(AssetFinder.AssetToUri("images/bg1.jpg"));
+        }
+
         private void WakeUpDetectorOnWakeUpConfirmedEvent()
         {
-            StopAudio();
+            _musicPlayer.StopMusic();
             _wakeUpDetector.Dispose();
             DoWakeupRoutine();
         }
 
         private void WakeUpDetectorOnWakeUpProgressEvent(short progress, short max)
         {
-            if (_waveOut != null)
-                _waveOut.Volume = (float)(1.0 - ((float)progress / max) * 1.0); //Adjust volume for fade
-        }
-
-        /// <summary>
-        /// Plays an mp3 file, with optional looping capabilities.
-        /// </summary>
-        /// <param name="fileUri">The path to the mp3 file.</param>
-        /// <param name="loop">True if the mp3 should loop continuously.</param>
-        /// <returns>Duration of the mp3 file.</returns>
-        private TimeSpan PlayAudio(Uri fileUri, bool loop)
-        {
-            _waveOut?.Dispose();
-            _waveOut = new WaveOut();
-
-            var reader = loop ? new Mp3Looper(Application.GetContentStream(fileUri)?.Stream) : new Mp3FileReader(Application.GetContentStream(fileUri)?.Stream);
-            var totalTime = reader.TotalTime;
-
-            _waveOut.Volume = 1.0f;
-            _waveOut.Init(reader);
-            _waveOut.Play();
-
-            return totalTime;
-        }
-
-        private void StopAudio()
-        {
-            _waveOut?.Stop();
-        }
-
-        private void DoWakeupRoutine()
-        {
-            TimeSpan totalTime = PlayAudio(AssetFinder.AssetToUri("audio/Intro.mp3"), false); //Start playing intro music.
-            Background = new SolidColorBrush(Colors.White);
-            BackgroundImage.Visibility = Visibility.Visible;
-            BackgroundImage.Source = new BitmapImage(AssetFinder.AssetToUri("images/bg1.jpg"));
+            _musicPlayer.SetVolume((float) (1.0 - ((float) progress/max)*1.0)); //Adjust volume for fade
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
---
+++ AlarmClock/Utilities/MusicPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using NAudio.Wave;
+
+namespace AlarmClock.Utilities
+{
+    public class MusicPlayer : IDisposable
+    {
+        private WaveOut _waveOut;
+
+        /// <summary>
+        /// Plays an mp3 file, with optional looping capabilities.
+        /// </summary>
+        /// <param name="audioAssetName">The name of the mp3 asset.</param>
+        /// <param name="loop">True if the mp3 should loop continuously.</param>
+        /// <returns>Duration of the mp3 file.</returns>
+        public TimeSpan PlayMp3Asset(string audioAssetName, bool loop)
+        {
+            _waveOut?.Dispose();
+            _waveOut = new WaveOut();
+
+            var reader = loop
+                ? new Mp3Looper(Application.GetContentStream(AssetFinder.AssetToUri("audio/" + audioAssetName))?.Stream)
+                : new Mp3FileReader(
+                    Application.GetContentStream(AssetFinder.AssetToUri("audio/" + audioAssetName))?.Stream);
+            var totalTime = reader.TotalTime;
+
+            _waveOut.Volume = 1.0f;
+            _waveOut.Init(reader);
+            _waveOut.Play();
+
+            return totalTime;
+        }
+
+        public void StopMusic()
+        {
+            _waveOut?.Stop();
+        }
+
+        public void SetVolume(float volume)
+        {
+            if (volume < 0) volume = 0;
+            if (volume > 1.0) volume = 1.0f;
+
+            if (_waveOut != null)
+                _waveOut.Volume = volume;
+        }
+
+        public void Dispose()
+        {
+            StopMusic();
+            _waveOut?.Dispose();
+        }
+    }
+}
