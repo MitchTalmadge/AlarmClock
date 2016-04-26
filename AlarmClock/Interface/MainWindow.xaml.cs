f20f6bf5b812b03a30d2af3d03974e73c081eeda
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-04-25 21:38:24 -0600

    Moved all Kinect detection algorithms into WakeUpDetector.cs

--- AlarmClock/Interface/MainWindow.xaml.cs
+++ AlarmClock/Interface/MainWindow.xaml.cs
@@ -13,49 +13,8 @@ namespace AlarmClock.Interface
     /// </summary>
     public partial class MainWindow
     {
-        /// <summary>
-        /// The required decrease in distance to trigger a success for a pixel (In millimeters).
-        /// </summary>
-        private const int RequiredDistanceDecrease = 700;
 
-        /// <summary>
-        /// The number of pixels which match REQUIRED_DISTANCE_DECREASE needed in order to trigger a success.
-        /// </summary>
-        private const int RequiredPixelDistanceMatches = 500;
-
-        /// <summary>
-        /// The change in distance required to trigger a fluctuation for a pixel (In millimeters).
-        /// </summary>
-        private const int RequiredFluctuationChange = 1000;
-
-        /// <summary>
-        /// The maximum number of fluctuations allowed for a pixel before disregarding it entirely.
-        /// </summary>
-        private const int MaxFluctuationTriggers = 30;
-
-        /// <summary>
-        /// The number of successes required before an overall success is triggered. (1 success may be achieved per second.)
-        /// </summary>
-        private const int SuccessCountRequired = 150;
-
-        /// <summary>
-        /// The number of successes
-        /// </summary>
-        private short _successCount;
-
-        //------------------------ Kinect ------------------------//
-
-        /// <summary>
-        /// The Kinect Sensor that is currently being used.
-        /// </summary>
-        private KinectSensor _kinectSensor;
-
-        private DepthImagePixel[] _initialPixels;
-        private bool _initialSet;
-
-        private DepthImagePixel[] _lastPixels;
-        private short[] _fluctuationCount;
-        private DepthImagePixel[] _currentPixels;
+        private WakeUpDetector _wakeUpDetector;
 
         //------------------------ Audio ------------------------//
 
@@ -68,7 +27,12 @@ namespace AlarmClock.Interface
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (LoadKinectSensor())
+            Background = new SolidColorBrush(Colors.Black);
+            _wakeUpDetector = new WakeUpDetector();
+            _wakeUpDetector.WakeUpConfirmedEvent += WakeUpDetectorOnWakeUpConfirmedEvent;
+            _wakeUpDetector.WakeUpProgressEvent += WakeUpDetectorOnWakeUpProgressEvent;
+
+            if (_wakeUpDetector.StartDetection())
             {
                 Background = new SolidColorBrush(Colors.Black);
 
@@ -77,50 +41,22 @@ namespace AlarmClock.Interface
             }
             else //Could not load Kinect Sensor. Just skip it.
             {
+                _wakeUpDetector.Dispose();
                 DoWakeupRoutine();
             }
         }
 
-        /// <summary>
-        /// Loads the first connected kinect sensor if one is connected.
-        /// </summary>
-        /// <returns>true if connection was established and the Kinect has started; false if otherwise.</returns>
-        private bool LoadKinectSensor()
+        private void WakeUpDetectorOnWakeUpConfirmedEvent()
         {
-            foreach (var sensor in KinectSensor.KinectSensors)
-            {
-                if (sensor.Status != KinectStatus.Connected) continue;
-
-                _kinectSensor = sensor;
-                break;
-            }
-
-            if (_kinectSensor == null) return false;
-
-            _kinectSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-            _initialPixels = new DepthImagePixel[_kinectSensor.DepthStream.FramePixelDataLength];
-            _currentPixels = new DepthImagePixel[_initialPixels.Length];
-            _fluctuationCount = new short[_initialPixels.Length];
-
-            _kinectSensor.DepthFrameReady += SensorDepthFrameReady;
-
-            try
-            {
-                _kinectSensor.Start();
-                return true;
-            }
-            catch (System.IO.IOException)
-            {
-                _kinectSensor = null;
-            }
-            return false;
+            StopAudio();
+            _wakeUpDetector.Dispose();
+            DoWakeupRoutine();
         }
 
-        private void ShutdownKinectSensor()
+        private void WakeUpDetectorOnWakeUpProgressEvent(short progress, short max)
         {
-            _kinectSensor?.Stop();
-            _kinectSensor?.Dispose();
-            _kinectSensor = null;
+            if (_waveOut != null)
+                _waveOut.Volume = (float)(1.0 - ((float)progress / max) * 1.0); //Adjust volume for fade
         }
 
         /// <summary>
@@ -151,70 +87,13 @@ namespace AlarmClock.Interface
 
         private void DoWakeupRoutine()
         {
-            StopAudio(); //Stop playing alarm
-            ShutdownKinectSensor(); //Shutdown Kinect sensor; we're done with it.
-
             TimeSpan totalTime = PlayAudio("assets/audio/Intro.mp3", false); //Start playing intro music.
             Background = new SolidColorBrush(Colors.White);
         }
 
-        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
-        {
-            using (var depthFrame = e.OpenDepthImageFrame())
-            {
-                if (depthFrame == null) return;
-
-                if (!_initialSet)
-                {
-                    depthFrame.CopyDepthImagePixelDataTo(_initialPixels);
-                    _initialSet = true;
-                }
-
-                depthFrame.CopyDepthImagePixelDataTo(_currentPixels);
-                if (_lastPixels == null)
-                    _lastPixels = (DepthImagePixel[])_currentPixels.Clone();
-
-                int pixelDistanceMatchCount = 0;
-                for (int i = 0; i < _currentPixels.Length; i++)
-                {
-                    if (_fluctuationCount[i] >= MaxFluctuationTriggers)
-                        continue; //This pixel fluctuates too much. Skip it.
-
-                    if (Math.Abs(_lastPixels[i].Depth - _currentPixels[i].Depth) >= RequiredFluctuationChange ||
-                        !_currentPixels[i].IsKnownDepth)
-                    {
-                        _fluctuationCount[i]++; //This pixel fluctuated too much. Add one to the counter.
-                        continue; //Skip it.
-                    }
-
-                    if (_initialPixels[i].Depth - _currentPixels[i].Depth >= RequiredDistanceDecrease)
-                        pixelDistanceMatchCount++;
-                }
-
-                if (pixelDistanceMatchCount >= RequiredPixelDistanceMatches)
-                {
-                    _successCount++;
-                    _waveOut.Volume = (float)(1.0 - ((float)_successCount / SuccessCountRequired) * 1.0); //Adjust volume for fade
-
-                    if (_successCount >= SuccessCountRequired) //Enough successes;  Start main wakeup routine and shutdown Kinect.
-                    {
-                        DoWakeupRoutine();
-                        return;
-                    }
-                }
-                else //Lost sight of user; must have laid back down. Reset volume and success counter.
-                {
-                    _successCount = 0;
-                    _waveOut.Volume = 1.0f;
-                }
-
-                _lastPixels = (DepthImagePixel[])_currentPixels.Clone();
-            }
-        }
-
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            ShutdownKinectSensor();
+            _wakeUpDetector?.Dispose();
         }
     }
 }
---
+++ AlarmClock/Utilities/WakeUpDetector.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Kinect;
+
+namespace AlarmClock.Utilities
+{
+    public class WakeUpDetector : IDisposable
+    {
+        /// <summary>
+        /// The required decrease in distance to trigger a success for a pixel (In millimeters).
+        /// </summary>
+        private const int RequiredDistanceDecrease = 700;
+
+        /// <summary>
+        /// The number of pixels which match REQUIRED_DISTANCE_DECREASE needed in order to trigger a success.
+        /// </summary>
+        private const int RequiredPixelDistanceMatches = 500;
+
+        /// <summary>
+        /// The change in distance required to trigger a fluctuation for a pixel (In millimeters).
+        /// </summary>
+        private const int RequiredFluctuationChange = 1000;
+
+        /// <summary>
+        /// The maximum number of fluctuations allowed for a pixel before disregarding it entirely.
+        /// </summary>
+        private const int MaxFluctuationTriggers = 30;
+
+        /// <summary>
+        /// The number of successes required before an overall success is triggered. (1 success may be achieved per second.)
+        /// </summary>
+        private const int SuccessCountRequired = 150;
+
+        /// <summary>
+        /// The number of successes
+        /// </summary>
+        private short _successCount;
+
+        //------------------------ Kinect ------------------------//
+
+        /// <summary>
+        /// The Kinect Sensor that is currently being used.
+        /// </summary>
+        private KinectSensor _kinectSensor;
+
+        private DepthImagePixel[] _initialPixels;
+        private bool _initialSet;
+
+        private DepthImagePixel[] _lastPixels;
+        private short[] _fluctuationCount;
+        private DepthImagePixel[] _currentPixels;
+
+        public delegate void WakeUpConfirmed();
+
+        public delegate void WakeUpProgress(short progress, short max);
+
+        /// <summary>
+        /// Starts Wake Up Detection.
+        /// </summary>
+        /// <returns>True if detection started successfully, False if it could not be started (Could not connect to sensor)</returns>
+        public bool StartDetection()
+        {
+            return LoadKinectSensor();
+        }
+
+        /// <summary>
+        /// Loads the first connected kinect sensor if one is connected.
+        /// </summary>
+        /// <returns>true if connection was established and the Kinect has started; false if otherwise.</returns>
+        private bool LoadKinectSensor()
+        {
+            foreach (var sensor in KinectSensor.KinectSensors)
+            {
+                if (sensor.Status != KinectStatus.Connected) continue;
+
+                _kinectSensor = sensor;
+                break;
+            }
+
+            if (_kinectSensor == null) return false;
+
+            _kinectSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            _initialPixels = new DepthImagePixel[_kinectSensor.DepthStream.FramePixelDataLength];
+            _currentPixels = new DepthImagePixel[_initialPixels.Length];
+            _fluctuationCount = new short[_initialPixels.Length];
+
+            _kinectSensor.DepthFrameReady += SensorDepthFrameReady;
+
+            try
+            {
+                _kinectSensor.Start();
+                return true;
+            }
+            catch (System.IO.IOException)
+            {
+                _kinectSensor = null;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Shuts down the Kinect Sensor and releases all streams.
+        /// </summary>
+        private void ShutdownKinectSensor()
+        {
+            _kinectSensor?.Stop();
+            _kinectSensor?.Dispose();
+            _kinectSensor = null;
+        }
+
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (var depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame == null) return;
+
+                if (!_initialSet)
+                {
+                    depthFrame.CopyDepthImagePixelDataTo(_initialPixels);
+                    _initialSet = true;
+                }
+
+                depthFrame.CopyDepthImagePixelDataTo(_currentPixels);
+                if (_lastPixels == null)
+                    _lastPixels = (DepthImagePixel[]) _currentPixels.Clone();
+
+                int pixelDistanceMatchCount = 0;
+                for (int i = 0; i < _currentPixels.Length; i++)
+                {
+                    if (_fluctuationCount[i] >= MaxFluctuationTriggers)
+                        continue; //This pixel fluctuates too much. Skip it.
+
+                    if (Math.Abs(_lastPixels[i].Depth - _currentPixels[i].Depth) >= RequiredFluctuationChange ||
+                        !_currentPixels[i].IsKnownDepth)
+                    {
+                        _fluctuationCount[i]++; //This pixel fluctuated too much. Add one to the counter.
+                        continue; //Skip it.
+                    }
+
+                    if (_initialPixels[i].Depth - _currentPixels[i].Depth >= RequiredDistanceDecrease)
+                        pixelDistanceMatchCount++;
+                }
+
+                if (pixelDistanceMatchCount >= RequiredPixelDistanceMatches)
+                {
+                    _successCount++;
+                    WakeUpProgressEvent?.Invoke(_successCount, SuccessCountRequired);
+                    //_waveOut.Volume = (float)(1.0 - ((float)_successCount / SuccessCountRequired) * 1.0); //Adjust volume for fade
+
+                    if (_successCount >= SuccessCountRequired)
+                        //Enough successes;  Start main wakeup routine and shutdown Kinect.
+                    {
+                        WakeUpConfirmedEvent?.Invoke();
+                        return;
+                    }
+                }
+                else //Lost sight of user; must have laid back down. Reset volume and success counter.
+                {
+                    _successCount = 0;
+                    WakeUpProgressEvent?.Invoke(_successCount, SuccessCountRequired);
+                    //_waveOut.Volume = 1.0f;
+                }
+
+                _lastPixels = (DepthImagePixel[]) _currentPixels.Clone();
+            }
+        }
+
+        public event WakeUpConfirmed WakeUpConfirmedEvent;
+
+        public event WakeUpProgress WakeUpProgressEvent;
+
+        public void Dispose()
+        {
+            ShutdownKinectSensor();
+        }
+    }
+}
